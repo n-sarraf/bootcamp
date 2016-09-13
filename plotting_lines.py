2d3bd19b6bcacbb4fdf2dbdd5e57b40c0561f195
Namita Sarraf <nsarraf@caltech.edu>
2016-09-13 14:24:35 -0700

    Plot spike data

--- plotting_lines.py
+++ plotting_lines.py
@@ -15,4 +15,18 @@ plt.plot(x, norm_I, marker='.', linestyle='none')
 plt.margins(0.02)
 plt.xlabel('$x$')
 plt.ylabel('$I(x) / I_0$')
+
+# Processing the spike data.
+data = np.loadtxt('data/retina_spikes.csv', skiprows=2, delimiter=',')
+t = data[:,0]
+V = data[:,1]
+
+# Close all other plots, just in case.
+plt.close()
+
+# Plot spike data.
+plt.plot(t, V)
+plt.xlabel('t (ms)')
+plt.ylabel('V (µV)')
+plt.xlim(1395,1400)
 plt.show()
