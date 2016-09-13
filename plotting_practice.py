85454f423bad13f49133ea4e0ec3432f8a7dfda8
Namita Sarraf <nsarraf@caltech.edu>
2016-09-13 15:05:34 -0700

    Plot as semilogy

--- plotting_practice.py
+++ plotting_practice.py
@@ -14,10 +14,20 @@ data_txt = np.loadtxt('data/collins_switch.csv', delimiter=',', skiprows=2)
 iptg = data_txt[:,0]
 gfp = data_txt[:,1]
 
+# # Plot ipgt vs gfp
+# plt.semilogx(iptg, gfp, linestyle='none', marker='.', markersize=20)
+# plt.xlabel('IPTG (mM)')
+# plt.ylabel('Normalized GFP')
+# plt.title('IPTG Titration - semilogx')
+# plt.ylim(-0.02, 1.02)
+# plt.xlim(8e-4, 15)
+# plt.show()
+
 # Plot ipgt vs gfp
-plt.semilogx(iptg, gfp, linestyle='none', marker='.', markersize=20)
+plt.semilogy(iptg, gfp, linestyle='none', marker='.', markersize=20)
 plt.xlabel('IPTG (mM)')
 plt.ylabel('Normalized GFP')
+plt.title('IPTG Titration - semilogy')
 plt.ylim(-0.02, 1.02)
 plt.xlim(8e-4, 15)
 plt.show()
