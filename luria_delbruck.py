7172d96afbb60616edfadbaf1ad203200de95c74
Namita Sarraf <nsarraf@caltech.edu>
2016-09-14 15:56:34 -0700

    Panda exercise

--- luria_delbruck.py
+++ luria_delbruck.py
@@ -21,3 +21,38 @@ ai_samples = np.random.binomial(n_cells, r, size=100000)
 print('AI mean: ', np.mean(ai_samples))
 print('AI std: ', np.std(ai_samples))
 print('AI Fano: ', np.var(ai_samples) / np.mean(ai_samples))
+
+
+# Function to draw out of random mutation hypothesis
+def draw_random_mutation(n_gen, r):
+    '''
+    Draw sample under random mutation hypothesis.
+    '''
+
+    # Initialize number of mutations
+    n_mut = 0
+
+    # Update number of mutations
+    for g in range(n_gen):
+        n_mut = 2*n_mut + np.random.binomial(2**g - 2*n_mut, r)
+
+    return n_mut
+
+
+def sample_random_mutation(n_gen, r, size=1):
+
+    # Initialize samples
+    samples = np.empty(size)
+
+    # Draw the samples
+    for i in range(size):
+        samples[i] = draw_random_mutation(n_gen, r)
+
+    return samples
+
+rm_samples = sample_random_mutation(n_gen, r, size=100000)
+
+# Report mean and std
+print('RM mean:', np.mean(rm_samples))
+print('RM std:', np.std(rm_samples))
+print('RM Fano:', np.var(rm_samples) / np.mean(rm_samples))
