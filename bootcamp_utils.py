a85d2b24f7b2c0f265c929455063b31683fe759e
Namita Sarraf <nsarraf@caltech.edu>
2016-09-14 12:00:47 -0700

    Add draw_bs_reps util

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def ecdf(data):
     '''
     Compute x, y values for an empirical distribution function
@@ -13,3 +14,19 @@ def ecdf(data):
     y = np.arange(1, 1+len(x)) / len(x)
 
     return x, y
+
+
+def draw_bs_reps(data, func, size=1):
+    '''
+    Generate bootstrap replicates from an experimental data set.
+    func refers to the statistical operation you would like to perform. ex: std, median, mean.
+    '''
+
+    # Draw bootstrap sample and store in array
+    n=len(data)
+    reps = np.empty(size)
+    for i in range (size):
+        bs_sample = np.random.choice(data, replace=True, size=n)
+        reps[i] = func(bs_sample)
+
+    return reps
