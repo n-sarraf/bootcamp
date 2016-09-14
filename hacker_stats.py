4eaaa953caf80fb432119446919b1b03902c4149
Namita Sarraf <nsarraf@caltech.edu>
2016-09-14 12:00:33 -0700

    Draw bootstrap data and calculate confidence intervals

--- hacker_stats.py
+++ hacker_stats.py
@@ -7,12 +7,31 @@ sns.set()
 bd_1975 = np.loadtxt('data/beak_depth_scandens_1975.csv')
 bd_2012 = np.loadtxt('data/beak_depth_scandens_2012.csv')
 
-x_1975, y_1975 = bootcamp_utils.ecdf(bd_1975)
-x_2012, y_2012 = bootcamp_utils.ecdf(bd_2012)
+# Generate bootstrap replicates for 1975 data
+n_reps = 100000
+bs_replicates_1975 = np.empty(n_reps)
+for i in range(n_reps):
+    bs_sample = np.random.choice(bd_1975, replace=True, size=len(bd_1975))
+    bs_replicates_1975[i] = np.std(bs_sample)
 
-plt.plot(x_1975, y_1975, marker='.', linestyle='none')
-plt.plot(x_2012, y_2012, marker='.', linestyle='none')
-plt.xlabel('beak depth (mm)')
-plt.ylabel('ECDF')
-plt.legend(('1975', '2012'), loc='lower right')
-plt.show()
+conf_int_1975 = np.percentile(bs_replicates_1975, [2.5, 97.5])
+
+# Generate bootstrap replicates for 2012 data
+n_reps = 100000
+bs_replicates_2012 = np.empty(n_reps)
+for i in range(n_reps):
+    bs_sample = np.random.choice(bd_2012, replace=True, size=len(bd_2012))
+    bs_replicates_2012[i] = np.std(bs_sample)
+
+conf_int_2012 = np.percentile(bs_replicates_2012, [2.5, 97.5])
+
+# x_1975, y_1975 = bootcamp_utils.ecdf(bd_1975)
+# x_2012, y_2012 = bootcamp_utils.ecdf(bd_2012)
+# x_1975_bs, y_1975_bs = bootcamp_utils.ecdf(bs_sample)
+#
+# plt.plot(x_1975, y_1975, marker='.', linestyle='none')
+# plt.plot(x_1975_bs, y_1975_bs, marker='.', linestyle='none', alpha=.5)
+# plt.xlabel('beak depth (mm)')
+# plt.ylabel('ECDF')
+# plt.legend(('1975', 'bootstrap'), loc='lower right')
+# plt.show()
