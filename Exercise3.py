bd3e420e2e7fd6b113ae83302866e7f921cdea5d
Namita Sarraf <nsarraf@caltech.edu>
2016-09-13 20:41:41 -0700

    Exercise 3.2c&d - plot real vs theoretical fold change

--- Exercise3.py
+++ Exercise3.py
@@ -33,3 +33,38 @@ plt.ylabel('Fold change')
 plt.title('IPTG Titration')
 plt.legend(('WT', 'Q18M', 'Q18A'), loc='lower right')
 plt.show()
+
+
+def fold_change(c, RK, KdA=.017, KdI=.002, Kswitch=5.8):
+    '''
+    Compute theoretical fold change of mutant.
+    '''
+
+    num = RK*(1+c/KdA)**2
+    denom = ((1+c/KdA)**2 + Kswitch*(1+c/KdI)**2)
+    fc = (1+num/denom)**-1
+
+    return fc
+
+# Create set of data for IPTG concentration
+c_wt = np.logspace(1e-6, 15, 500)
+c_q18m = np.logspace(1e-6, 15, 500)
+c_q18a = np.logspace(1e-6, 15, 500)
+
+plt.close()
+
+# Plot theoretical and real data on same plot
+wt_tfc = fold_change(c_wt, 141.5)
+q18m_tfc = fold_change(c_q18m, 1332)
+q18a_tfc = fold_change(c_q18a, 16.56)
+plt.semilogx(wt_iptg, wt_fc, linestyle='none', marker='.', markersize=20)
+plt.semilogx(q18m_iptg, q18m_fc, linestyle='none', marker='.', markersize=20)
+plt.semilogx(q18a_iptg, q18a_fc, linestyle='none', marker='.', markersize=20)
+plt.semilogx(c_wt, wt_tfc)
+plt.semilogx(c_q18m, q18m_tfc)
+plt.semilogx(c_q18a, q18a_tfc)
+plt.xlabel('IPTG (mM)')
+plt.ylabel('Fold change')
+plt.title('IPTG Titration')
+plt.legend(('WT', 'Q18M', 'Q18A', 'Th WT', 'Th Q18M', 'Th Q18A'), loc='upper left')
+plt.show()
