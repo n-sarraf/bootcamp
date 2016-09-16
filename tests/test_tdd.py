2604ebde72c38d323dcc491142c897d3ffb8acee
Namita Sarraf <nsarraf@caltech.edu>
2016-09-15 20:51:59 -0700

    Test driven development test file

---
+++ tests/test_tdd.py
@@ -0,0 +1,15 @@
+import pytest
+
+import tdd
+
+def test_n_neg():
+    assert tdd.n_neg('E') == 1
+    assert tdd.n_neg('D') == 1
+    assert tdd.n_neg('') == 0
+    assert tdd.n_neg('ACKLWTTAE') == 1
+    assert tdd.n_neg('DEDEEDDEE') == 9
+    assert tdd.n_neg('acklwttae') == 1
+
+    pytest.raises(RuntimeError, "tdd.n_neg('Z')")
+
+    return None
