d46369e253610239182e11c5a763331da2d4817b
Namita Sarraf <nsarraf@caltech.edu>
2016-09-15 20:50:43 -0700

    Update

--- plotting_practice.py
+++ plotting_practice.py
@@ -13,21 +13,21 @@ data_txt = np.loadtxt('data/collins_switch.csv', delimiter=',', skiprows=2)
 # Slice out iptg and gfp
 iptg = data_txt[:,0]
 gfp = data_txt[:,1]
-#
-# # Plot ipgt vs gfp
-# plt.semilogx(iptg, gfp, linestyle='none', marker='.', markersize=20)
-# plt.xlabel('IPTG (mM)')
-# plt.ylabel('Normalized GFP')
-# plt.title('IPTG Titration - semilogx')
-# plt.ylim(-0.02, 1.02)
-# plt.xlim(8e-4, 15)
-# plt.show()
+
+# Plot ipgt vs gfp
+plt.semilogx(iptg, gfp, linestyle='none', marker='.', markersize=20)
+plt.xlabel('IPTG (mM)')
+plt.ylabel('Normalized GFP')
+plt.title('IPTG Titration - semilogx')
+plt.ylim(-0.02, 1.02)
+plt.xlim(8e-4, 15)
+plt.show()
 
 # Slice out iptg and gfp
 sem = data_txt[:,2]
 
 # Plot ipgt vs gfp
-plt.errorbar(iptg, gfp, yerr=sem, linestyle='none', marker='.', markersize=20)
+plt.errorbar(iptg, gfp, yerr=sem, linestyle='none', marker='.', markersize=10)
 plt.xlabel('IPTG (mM)')
 plt.ylabel('Normalized GFP')
 plt.title('IPTG Titration - semilogx')
@@ -35,3 +35,5 @@ plt.ylim(-0.02, 1.02)
 plt.xlim(8e-4, 15)
 plt.xscale('log')
 plt.show()
+
+Practice exercise 2
